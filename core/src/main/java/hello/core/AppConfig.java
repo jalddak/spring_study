7643ac927b2f3ea9b7e1bd6765f6b8180430e451
jalddak <jalddak98@gmail.com>
2023-12-07 22:58:55 +0900

    싱글톤 컨테이너

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -11,21 +11,30 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// Configuration 이 없어지면 스프링 컨테이너 관리가 안된다. 그래서 bean 어노테이션이 있어도 관리가 안된다. (싱글톤 보장 x)
+// 따라서 new 하는 만큼 생성해낸다.
 @Configuration
 public class AppConfig {
 
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
+    // 그러면 두번 new (생성) 하니까 싱글톤이 깨지지 않을까?
+
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
